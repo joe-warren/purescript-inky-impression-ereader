b3181b7078482a9ccbfb5a959adaa4fc6b37a054
Joseph Warren <hungryjoe.warren@gmail.com>
2022-09-23 02:15:48 +0100

    This actually works

---
+++ python_modules/buttons.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+import threading as threading
+import queue
+buttons = [5, 6, 16, 24]
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(buttons, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+q = queue.Queue(maxsize=100);
+
+def cb(pin):
+    q.put([buttons.index(pin)+1, GPIO.input(pin) == 0])
+
+for pin in buttons:
+    GPIO.add_event_detect(pin, GPIO.BOTH, cb)
+
+def next():
+    return q.get()
--- src/RawRPiButtons.js
+++ src/RawRPiButtons.js
@@ -2,27 +2,13 @@ const {python} = require('pythonia');
 
 exports.runButtonsRaw = function(callback){
    async function go() {    
-    const gpio = await python('RPi.GPIO')
-    const signal = await python('signal')
-    const pins = [5, 6, 16, 24]
-
-    const bcm = await gpio.BCM
-    await gpio.setmode(bcm)
-    const iin = await gpio.IN;
-    const pud_up = await gpio.PUD_UP
-    await gpio.setup$(pins, iin, {pull_up_down: pud_up });
-    const makeCallback = function(i, isDown){
-        return callback(i)(isDown)
-    }
-    const both = await gpio.BOTH
-    const falling = await gpio.FALLING
-    
-    const cb = function (pin, edge) {
-       callback(pins.indexOf(pin) + 1)(edge == falling)()
-    }
-
-    for(i = 1; i <=4; i++){
-       await gpio.add_event_detect(pins[i-1], both, cb);
+    const buttons = await python('./buttons.py')
+    while(true){
+        const r = await buttons.next();
+        const [pi, pdn] = r;
+        const i = await pi;
+        const dn = await pdn;
+        callback(i)(dn)();
     }
     //await (new Promise (x => {}))
     //python.exit() // Make sure to exit Python in the end to allow node to exit. You can also use process.exit.
