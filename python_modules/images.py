cf4aacdcead2025277e988dc6ad54a330cae313f
Joseph Warren <hungryjoe.warren@gmail.com>
2022-09-26 04:00:42 +0100

    use quantize

--- python_modules/images.py
+++ python_modules/images.py
@@ -25,12 +25,15 @@ def openPalettized(path):
 
 def openAndResizeArbitrary(w, h, path):
     try:
-        img = Image.open(path).resize([w, h])
+        img = Image.open(path).resize([w, h]).convert('RGB')
 
         pImg = Image.new('P', [w, h])
         pImg.putpalette(palette + [0, 0, 0]*(256-8))
         pImg.paste(img, (0, 0))
-        return pImg
+        palimage = Image.new('P', (16, 16))
+        palimage.putpalette(palette + [0, 0, 0]*(256-8))
+        newimage = img.quantize(palette=palimage)
+        return newimage
     except FileNotFoundError as e:
         return f"couldn't find ${path} (got ${e})"
 
